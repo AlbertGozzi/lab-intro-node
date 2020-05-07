009f512e328376f727059917e3a3034a6d095ef1
Albert Gozzi <albertgozzi@gmail.com>
2020-05-07 12:39:41 -0400

    Done

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let positionToAdd = this.items.findIndex(number => number > item);
+    if (positionToAdd === -1) {
+      this.items.push(item);
+    } else {
+      this.items.splice(positionToAdd, 0, item);
+    }
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) { return Error(OutofBounds); }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) { return Error(EmptySortedList); }
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) { return Error(EmptySortedList); }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) { return Error(EmptySortedList); }
+    return this.sum()/this.length;
+  }
 }
 
 module.exports = SortedList;
